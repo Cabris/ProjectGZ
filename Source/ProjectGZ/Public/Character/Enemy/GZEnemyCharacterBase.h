7a7352c2bf36efa7f5d083a6531e68be422bb37d
Christ Chen <christtabris@gmail.com>
2025-06-06 22:28:46 +0800

    add IMC, IA(move), base character class, player class, enemy class.

---
+++ Source/ProjectGZ/Public/Character/Enemy/GZEnemyCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/GZCharacterBase.h"
+#include "GZEnemyCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGZ_API AGZEnemyCharacterBase : public AGZCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectGZ/Public/Character/Player/GZPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/GZCharacterBase.h"
+#include "GZPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGZ_API AGZPlayerCharacter : public AGZCharacterBase
+{
+	GENERATED_BODY()
+	
+};
