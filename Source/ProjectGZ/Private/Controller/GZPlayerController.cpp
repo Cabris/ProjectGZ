d72dba767a19362867e32a60a886ccd1f9465134
Christ Chen <christtabris@gmail.com>
2025-06-09 01:06:18 +0800

    update player controller

--- Source/ProjectGZ/Private/Controller/GZPlayerController.cpp
+++ Source/ProjectGZ/Private/Controller/GZPlayerController.cpp
@@ -2,4 +2,59 @@
 
 
 #include "Controller/GZPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include <EnhancedInputComponent.h>
+#include "InputActionValue.h"
 
+AGZPlayerController::AGZPlayerController()
+{
+	bReplicates = true; //for multiplayer usage
+}
+
+void AGZPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(GZContext);
+	auto inputSubsys=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(inputSubsys);
+	inputSubsys->AddMappingContext(GZContext,0);
+	bShowMouseCursor=false;
+	DefaultMouseCursor= EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AGZPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGZPlayerController::Move);
+}
+
+void AGZPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+}
+
+void AGZPlayerController::Move(const FInputActionValue& inputActionValue)
+{
+	FVector2D inputAxisVector = inputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, inputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, inputAxisVector.X);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No pawn controlled"));
+	}
+}
--- Source/ProjectGZ/Public/Controller/GZPlayerController.h
+++ Source/ProjectGZ/Public/Controller/GZPlayerController.h
@@ -9,9 +9,24 @@
 /**
  * 
  */
+class UInputMappingContext;;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class PROJECTGZ_API AGZPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AGZPlayerController();
+	virtual  void SetupInputComponent() override;
+	virtual  void BeginPlay() override;
+	virtual  void PlayerTick(float DeltaTime) override;
+protected:
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> GZContext;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	void Move(const FInputActionValue& inputActionValue);
 };
