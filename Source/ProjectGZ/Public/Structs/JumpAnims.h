d5a1b5a4987f39419aa2e68dc3094000c592ed90
Christ Chen <christtabris@gmail.com>
2025-07-10 21:20:46 +0800

    add landscape

---
+++ Source/ProjectGZ/Public/Structs/JumpAnims.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "JumpAnims.generated.h"
+
+USTRUCT(BlueprintType)
+struct FJumpAnims
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> JumpStartAnim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> JumpApexAnim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> JumpLoopAnim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> JumpRecoveryAnim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> FallLandAnim;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Jump Animation")
+	TObjectPtr<UAnimSequence> FallLoopAnim;
+};
